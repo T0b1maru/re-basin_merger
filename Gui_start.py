2c361f3df4b25eede22119590ff1b74c8dfa8e62
T0b1maru <hey.ben@pm.me>
2023-03-25 12:00:33 +0100

    gui config and save/load last session

--- Gui_start.py
+++ Gui_start.py
@@ -1,28 +1,48 @@
 import gradio as gr
 import os
 import time
+import json
+
+# Define the filename to save the input values
+SAVE_FILE = "re-basin_config.json"
+
+# Define a function to save the input values to a file
+def save_input_values(inputs):
+    with open(SAVE_FILE, "w") as f:
+        json.dump(inputs, f)
+
+# Define a function to load the input values from a file
+def load_input_values():
+    if os.path.exists(SAVE_FILE):
+        with open(SAVE_FILE, "r") as f:
+            return json.load(f)
+    else:
+        return {}
+
+# Load saved input values (if any)
+saved_inputs = load_input_values()
 
 demo = gr.Blocks(title="Re-basin Merger")
 with demo:
     error_box = gr.Textbox(label="Error", visible=False)
 
     with gr.Row():
-        modelA_box = gr.Textbox(label="Model A path")
-        modelB_box = gr.Textbox(label="Model B path")
+        modelA_box = gr.Textbox(label="Model A path", value=saved_inputs.get("modelA_box", ""))
+        modelB_box = gr.Textbox(label="Model B path", value=saved_inputs.get("modelB_box", ""))
         
-    output_box = gr.Textbox(label="Filename for saving merged model or pruning", value="merged_model")
+    output_box = gr.Textbox(label="Filename for saving merged model or pruning", value=saved_inputs.get("output_box", "merged_model"))
 
     with gr.Row():
-        iterations_box = gr.Number(label="Iterations", value=100)
-        alpha_box = gr.Slider(value=0.5, minimum=0, maximum=1, step=0.001, label="Alpha")
+        iterations_box = gr.Number(label="Iterations", value=saved_inputs.get("iterations_box", 100))
+        alpha_box = gr.Slider(value=saved_inputs.get("alpha_box", 0.5), minimum=0, maximum=1, step=0.001, label="Alpha")
 
     with gr.Row():
-        usefp16_box = gr.Checkbox(label="Use fp16", value=True)
-        cuda_box = gr.Checkbox(label="GPU", value=False)
-        fast_box = gr.Checkbox(label="Fast", value=True)
+        usefp16_box = gr.Checkbox(label="Use fp16", value=saved_inputs.get("usefp16_box", True))
+        cuda_box = gr.Checkbox(label="GPU", value=saved_inputs.get("cuda_box", False))
+        fast_box = gr.Checkbox(label="Fast", value=saved_inputs.get("fast_box", True))
 
     with gr.Row():
-        merge_layers_radio = gr.Radio(["All", "Convolutional layers", "Fully connected layers"], label="Layers to be merged", value="All", interactive=True)
+        merge_layers_radio = gr.Radio(["All", "Convolutional layers", "Fully connected layers"], label="Layers to be merged", value=saved_inputs.get("merge_layers_radio", "All"), interactive=True)
 
     with gr.Row():
         install_btn = gr.Button("install re-basin requirements")
@@ -36,6 +56,19 @@ with demo:
         patient_summary_box = gr.Textbox(label="Patient Summary")
 
     def run_rebasin(modelA, modelB, output, iterations, alpha, usefp16, device, merge_layers, fast):
+        inputs = {
+            "modelA_box": modelA,
+            "modelB_box": modelB,
+            "output_box": output,
+            "iterations_box": iterations,
+            "alpha_box": alpha,
+            "usefp16_box": usefp16,
+            "cuda_box": device,
+            "merge_layers_radio": merge_layers,
+            "fast_box": fast
+        }
+        save_input_values(inputs)
+        
         if len(modelA) == 0:
             return {error_box: gr.update(value="Enter path to model A", visible=True)}
         if len(modelB) == 0:
